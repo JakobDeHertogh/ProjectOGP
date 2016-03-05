0dd8fa90fb1c311918b3282b9a5faa121d4b1444
Kristof Van Cappellen <kristofie.vancappellen@hotmail.com>
2016-03-05 17:32:44 +0100

    I hate advanceTime

--- src/hillbillies/model/Unit.java
+++ src/hillbillies/model/Unit.java
@@ -271,8 +271,23 @@ public class Unit {
 	/**
 	 * 
 	 * @param dt
+	 * 
 	 */
-	public void advanceTime(double dt){
+	public enum action{
+			MOVE, ATTACK, DEFEND, WORK
+		}
+	public void advanceTime(double dt) throws ModelException{
+		double[] newposition = new double[3];
+		newposition[0] = this.getXPosition() + dt * this.xspeed;
+		newposition[0] = this.getYPosition() + dt * this.yspeed;
+		newposition[0] = this.getZPosition() + dt * this.zspeed;
+		setPosition(newposition);
+		
+		//Fighting
+		
+		
+		//Working
+		
 		
 	}
 	/**
@@ -286,35 +301,34 @@ public class Unit {
 		// Eerst definieren we de positie waar we naartoe zullen bewegen.
 		double[] newposition = new double[3];
 		newposition[0]= this.getXPosition() + dx;
-		newposition[1]= this.getYPosition()+dy;
-		newposition[2]= this.getZPosition()+dz;
+		newposition[1]= this.getYPosition() + dy;
+		newposition[2]= this.getZPosition() + dz;
+		
+		// We kijken of die een mogelijke positie is, indien niet ModelException
 		if (!isValidPosition(newposition))
 			throw new ModelException();
+		
+		// Goede positie -> berekenen verplaatsingssnelheid en -vector berekenen;
 		double xdistance = Math.pow((newposition[0]-this.getXPosition()), 2);
 		double ydistance = Math.pow((newposition[1]-this.getYPosition()), 2);
 		double zdistance = Math.pow((newposition[2]-this.getZPosition()), 2);
+		
 		//We maken alle parameters klaar voor de verplaatsing naar een andere cube.
 		this.distance = Math.sqrt(xdistance + ydistance + zdistance);
 		setCurrentspeed(this.position, newposition);
-//		if (isSprinting())
-//			this.currentspeed = 2* this.currentspeed;
-// 		Dit zou ik opgelost hebben in de setCurrentSpeed methode
+		if (isSprinting())
+			this.currentspeed = 2* this.currentspeed;
 		setSpeedVector(xdistance, ydistance, zdistance, this.distance);
-		advanceTime(dt);
-		setPosition(newposition);
 	}
 
 	public void setCurrentspeed(double[] start, double[] end){
 		double dz = start[2]-end[2];
-		if (isWalking)
-			if (dz ==-1)
-				this.currentspeed = 0.5*vb;
-			else if (dz==1)
-				this.currentspeed = 1.2*vb;
-			else
-				this.currentspeed = vb;
-			if (isSprinting())
-				this.currentspeed = 2*vb;
+		if (dz ==-1)
+			this.currentspeed = 0.5*vb;
+		else if (dz==1)
+			this.currentspeed = 1.2*vb;
+		else
+			this.currentspeed = vb;
 	}
 	public void setSpeedVector(double xdistance, double ydistance, double zdistance, double totaldistance){
 		double[] speedvector = new double[3];
@@ -322,6 +336,9 @@ public class Unit {
 		speedvector[1] = this.currentspeed * ydistance / totaldistance;
 		speedvector[2] = this.currentspeed * zdistance / totaldistance;
 		this.speedVector = speedvector;
+		this.xspeed = speedvector[0];
+		this.yspeed = speedvector[1];
+		this.zspeed = speedvector[2];
 	}
 		
 	//Path finding method (zoals in bundel): opeenvolging van hierboven
@@ -474,7 +491,9 @@ public class Unit {
 	private double vb = 1.5* (this.agility+ this.strength) / (2*this.weight);
 	private double currentspeed;
 	private double[] speedVector;
-	private double dt = 0.2;
+	private double xspeed;
+	private double yspeed;
+	private double zspeed;
 	private int minXPos = 0;
 	private int maxXPos = 50;
 	private int minYPos = 0;
