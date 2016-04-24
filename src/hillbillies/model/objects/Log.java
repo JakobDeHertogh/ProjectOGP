ea4731552b4482fc94f55b093463460bf6900276
Kristof Van Cappellen <kristofie.vancappellen@hotmail.com>
2016-04-24 15:14:36 +0200

    Added documentation to Log class

--- src/hillbillies/model/objects/Log.java
+++ src/hillbillies/model/objects/Log.java
@@ -3,7 +3,16 @@ package hillbillies.model.objects;
 import hillbillies.model.world.Cube;
 import hillbillies.model.world.World;
 import ogp.framework.util.ModelException;
-
+/**
+ * A class of game Logs. Each Unit has a position and a weight. Logs can move and fall.
+ * @invar	The starting value for the weight of every Log must be valid.
+ * 			| isValidWeight(weight)
+ * 
+ * @version 0.51
+ * @author Kristof Van Cappellen
+ * @author Jakob De Herthogh
+ *
+ */
 public class Log {
 	
 	public Log (World world, int[] startPosition) throws ModelException{
@@ -20,6 +29,9 @@ public class Log {
 		this.fallingTo = this.getzPosition();
 	}
 	
+	/**
+	 * Returns the coordinate of the Cube which is occupied by this Log.
+	 */
 	public int[] getCubeCoordinate (){
 		int[] cubecoordinate = new int[3];
 		cubecoordinate[0] = (int)Math.floor(this.position[0]);
@@ -28,46 +40,87 @@ public class Log {
 		return cubecoordinate;
 	}
 	
-	
+	/**
+	 * Returns the Cube which is occupied by this Log.
+	 */
 	public Cube occupiesCube(){
 		return this.world.getCubeAtPos(this.getCubeCoordinate()[0], this.getCubeCoordinate()[1], this.getCubeCoordinate()[2]);
 	}
 	
+	
+	/**
+	 * Returns the Cube on top of which this Log is sitting.
+	 */
 	public Cube getCubeUnder(){
 		return this.world.getCubeAtPos(this.getCubeCoordinate()[0], this.getCubeCoordinate()[1], this.getCubeCoordinate()[2]-1);
 	}
 	
+	/**
+	 * Returns the World which the Log is situated in.
+	 */
 	public World getWorld(){
 		return this.world;
 	}
 	
+	/**
+	 * Checks whether the given position is a valid position for a Log.
+	 * @param position
+	 * @return true if and only if the Cube at the given position is of a passable type, 
+	 * 			and the Cube under that Cube is of unpassable type.
+	 * 			Else returns false. 
+	 */
 	public boolean isValidPosition(int [] position){
 		if ((this.occupiesCube().isPassableType())&&( ! this.getCubeUnder().isPassableType()))
 			return true;
 		return false;
 	}
 	
+	/**
+	 * 
+	 * @return The position of this Log.
+	 */
 	public double[] getPosition(){
 		return this.position;
 	}
 
-
+	/**
+	 * Sets the position of the Log to the given position.
+	 * @param newPosition
+	 *
+	 */
 	public void setPosition(double[] newPosition){
 		this.position = newPosition;
 	}
 	
+	/**
+	 * 
+	 * @return The z-value of the Log's position.
+	 */
 	public double getzPosition() {
 		return this.position[2];
 	}
-		
+	
+	/**
+	 * Checks whether the given weight is a valid option for a Log's weight.
+	 * @param weight
+	 * @return true if the given weight lies between the given lowerlimit and upperlimit.
+	 * @return false if the given weight does not lie between the given lowerlimit and upperlimit.
+	 */
 	public boolean isValidWeight(int weight){
 		return ((weight>lowerLimit)&&(weight<upperLimit));
 	}
-	
+	/**
+	 * 
+	 * @return The weight of this Log.
+	 */
 	public int getWeight() {
 		return this.weight;
 	}
-
+	/**
+	 * Sets the weight of this Log to the given weight.
+	 * @param weight
+	 * @throws ModelException if the given weight is not a valid weight for a Log.
+	 */
 	public void setWeight(int weight) throws ModelException{
 		if (!isValidWeight(weight)){
 			throw new ModelException("Invalid weight");
@@ -75,7 +128,10 @@ public class Log {
 		this.weight = weight;
 		
 	}
-	
+	/**
+	 * Adapts the Log's current position on whether it is falling or being carried.
+	 * @param dt
+	 */
 	public void advanceTime(double dt){
 		if (this.isCarriedBy != null)
 			this.position = this.isCarriedBy.getPosition();
@@ -91,11 +147,18 @@ public class Log {
 		}
 	}
 	
+	
+	/**
+	 * Removes the Log from the gameworld.
+	 */
 	public void terminate() {
 		this.isTerminated = true;
 		this.world.removeLog(this);
 	}
 	
+	/**
+	 * Checks whether this Log is terminated.
+	 */
 	public boolean isTerminated(){
 		return this.isTerminated;
 	}
