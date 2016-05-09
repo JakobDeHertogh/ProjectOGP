0a35c89027518a479b7e9037e3272f2037ba39e7
Kristof Van Cappellen <kristofie.vancappellen@hotmail.com>
2016-05-09 17:57:15 +0200

    Movement fixed?

--- src/hillbillies/model/Path.java
+++ src/hillbillies/model/Path.java
@@ -31,7 +31,7 @@ public class Path {
 			
 			for (Cube next: current.getSurroundingCubes()){
 				try {
-					double new_cost = Cost_so_far.get(current)+ 1;
+					double new_cost = Cost_so_far.get(current)+ Math.abs(next.getXPosition()-current.getXPosition()) +Math.abs(next.getZPosition()-current.getZPosition()) + Math.abs(next.getYPosition()-current.getYPosition());
 					if (((!Cost_so_far.containsKey(next))|| (new_cost < Cost_so_far.get(next))) && (next.isValidCube())){
 						Cost_so_far.put(next, new_cost);
 						double priority = new_cost + CalcCost(end, next);
@@ -61,26 +61,44 @@ public class Path {
 	public double CalcCost(Cube a, Cube b){
 		int[] aPos = a.getPosition();
 		int[] bPos = b.getPosition();
-		return Math.abs(aPos[0]-bPos[0]) + Math.abs(aPos[1]-bPos[1]) + Math.abs(aPos[2]-bPos[2]);
+		return Math.sqrt(Math.pow((aPos[0]-bPos[0]),2) + Math.pow((aPos[1]-bPos[1]),2) + Math.pow((aPos[2]-bPos[2]),2));
 	}
-	
+	/**
+	 * Returns the path.
+	 * @return
+	 */
 	public Stack<Cube> getRoute(){
 		return this.path;
 	}
 
-	
+	/**
+	 * Returns the starting cube of the path.
+	 * @return
+	 */
 	public Cube getStart(){
 		return this.start;
 	}
 	
+	/**
+	 * Sets the starting cube of the path.
+	 * @param start
+	 */
 	public void setStart(Cube start){
 		this.start = start;
 	}
 	
+	/**
+	 * Returns the end Cube of the path/
+	 * @return
+	 */
 	public Cube getEnd(){
 		return this.end;
 	}
 	
+	/**
+	 * Sets the end Cube of the path.
+	 * @param end
+	 */
 	public void setEnd(Cube end){
 		this.end = end;
 	}
